1b9c936c44121b652b3ba450eee6824ef7862c89
Mahek2710 <2023.mahek.hingorani@ves.ac.in>
2025-12-14 21:01:12 +0530

    Added modal UI and basic task creation functionality

--- script.js
+++ script.js
@@ -5,45 +5,69 @@ let dragElement = null;
 
 console.log(todo, progress, done);
 
-
 const tasks = document.querySelectorAll('.task');
 
 tasks.forEach(task => {
     task.addEventListener("drag", (e) => {
-        // console.log("dragging", e);
         dragElement = task;
     });
-})
-
-
+});
 
 function addDragEventsOnColumn(column) {
     column.addEventListener("dragenter", (e) => {
         e.preventDefault();
         column.classList.add("hover-over");
     });
+
     column.addEventListener("dragleave", (e) => {
         e.preventDefault();
         column.classList.remove("hover-over");
     });
-    
+
     column.addEventListener("dragover", (e) => {
         e.preventDefault();
     });
 
     column.addEventListener("drop", (e) => {
         e.preventDefault();
-        
-
-        console.log("dropped",dragElement, "in", column);
         column.appendChild(dragElement);
         column.classList.remove("hover-over");
-
     });
 }
-
-
 
 addDragEventsOnColumn(todo);
 addDragEventsOnColumn(progress);
 addDragEventsOnColumn(done);
+
+/* modal related logic */
+const toggleModalButton = document.querySelector('#toggle-modal');
+const modalBg = document.querySelector(".modal .bg");
+const modal = document.querySelector(".modal");
+const addTaskButton = document.querySelector("#add-new-task");
+
+toggleModalButton.addEventListener("click", () => {
+    modal.classList.toggle("active");
+});
+
+modalBg.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
+
+addTaskButton.addEventListener("click", () => {
+    const taskTitle = document.querySelector("#task-title-input").value;
+    const taskDesc = document.querySelector("#task-desc-input").value;
+
+    const div = document.createElement("div");
+    div.classList.add("task");
+    div.setAttribute("draggable", "true");
+
+    div.innerHTML = `
+        <h2 class="task-title">${taskTitle}</h2>
+        <p class="task-desc">${taskDesc}</p>
+        <button>Delete</button>
+    `;
+
+    todo.appendChild(div);
+    modal.classList.remove("active");
+});
+/* modal related logic */
